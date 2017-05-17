5841fa30ebc111b228236e322e996eb69c34e657
Witoo Harianto <witoo@plimble.com>
2017-05-18 05:13:42 +0700

    start id = 1

--- before-run.sh
+++ before-run.sh
@@ -3,6 +3,6 @@
 T_HOSTNAME=$(hostname)
 T_ID=$( echo ${T_HOSTNAME} | cut -d "-" -f2 | cut -d "-" -f1 )
 
-export KAFKA_BROKER_ID=$T_ID
+export KAFKA_BROKER_ID=$(( T_ID + 1 ))
 
 sh /etc/confluent/docker/run
